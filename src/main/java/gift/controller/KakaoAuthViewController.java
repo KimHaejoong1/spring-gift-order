bc765190b50737161d755c0e39f2c531db138cf4
KimHaejoong1 <hjkim2124@gmail.com>
2025-07-28 14:16:55 +0900

    refactor(view): KakaoAuthViewController loginUrl 재사용 개선

--- src/main/java/gift/controller/KakaoAuthViewController.java
+++ src/main/java/gift/controller/KakaoAuthViewController.java
@@ -1,6 +1,6 @@
 package gift.controller;
 
-import gift.config.KakaoProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/kakao")
 public class KakaoAuthViewController {
-    private final KakaoProperties kakaoProperties;
-
-    public KakaoAuthViewController(KakaoProperties kakaoProperties) {
-        this.kakaoProperties = kakaoProperties;
-    }
+    @Value("${kakao.login-url}")
+    private String loginUrl;
 
     @GetMapping("/login")
     public String login(Model model) {
-        String loginUrl = "https://kauth.kakao.com/oauth/authorize?scope=talk_message&response_type=code"
-                + "&redirect_uri=" + kakaoProperties.redirectUri()
-                + "&client_id=" + kakaoProperties.restApiKey();
         model.addAttribute("loginUrl", loginUrl);
         return "kakao-login";
     }
